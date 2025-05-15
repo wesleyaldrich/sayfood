aebf8da60c6b60bf20ba1ab0d476504aa6b5c339
Wesley Aldrich <169461537+wesleyaldrich@users.noreply.github.com>
2025-05-15 21:30:38 +0700

    feat: build the starting layout.
    for any page in the future, consider checking the home page to follow the same layout.
    
    bootstrap has been imported.

---
+++ resources/views/home.blade.php
@@ -0,0 +1,8 @@
+@extends('layout.app')
+@section('title', 'Home Page')
+@section('content')	
+<div class="container">
+    <h1>Welcome to the Home Page</h1>
+    <p>This is the dummy home page of our application.</p>
+</div>
+@endsection
---
+++ resources/views/layout/header.blade.php
@@ -0,0 +1,25 @@
+<header>
+    <div class="logo">
+        <a href="{{ url('/') }}">
+            <img src="{{ asset('assets/sayfood.png') }}" alt="Logo">
+        </a>
+    </div>
+    <div class="right">
+        <nav>
+            <ul>
+                <li><a href="{{ url('/') }}" class="oswald nav-button nav-active">HOME</a></li>
+                <li><a href="{{ url('/foods') }}" class="oswald nav-button">FOODS</a></li>
+                <li><a href="{{ url('/charity') }}" class="oswald nav-button">CHARITY</a></li>
+                <li><a href="{{ url('/activity') }}" class="oswald nav-button">ACTIVITY</a></li>
+            </ul>
+        </nav>
+        <div class="icons">
+            <a href="{{ url('/notifs') }}">
+                <img src="{{ asset('assets/icon_notif.png') }}" alt="Notification Icon" class="notif-icon-img">
+            </a>
+            <a href="{{ url('/profile') }}">
+                <img src="{{ asset('assets/icon_profile.png') }}" alt="Profile Icon" class="profile-icon-img">
+            </a>
+        </div>
+    </div>
+</header>
