1dbdb938ba37c1587c11439adda3bf92701bc03d
Wesley Aldrich <169461537+wesleyaldrich@users.noreply.github.com>
2025-05-22 15:13:53 +0700

    feat(auth-customer): implement UI for customer login and register page.
    while doing this, AuthController were created with no implementation yet.

---
+++ resources/views/login.blade.php
@@ -0,0 +1,44 @@
+@extends('layout.auth')
+@section('title', 'Sayfood | Login Page')
+@section('content')	
+    <div class="auth-form-container d-flex align-items-center justify-content-center flex-column p-3" >
+        <div class="container d-flex justify-content-center">
+            <img src="{{ asset('assets/sayfood.png') }}" alt="Sayfood Logo" class="logo">
+        </div>
+        
+        <h2 class="oswald mt-3 mb-2">LOGIN</h2>
+
+        <div class="container">
+            <form action=" {{ route('login') }} ">
+                @csrf
+
+                <div class="form-group mb-2">
+                    <label for="name" class="oswald">Username</label>
+                    <input type="text" class="form-control" id="username" name="username" required>
+                </div>
+
+                <div class="form-group mb-2">
+                    <label for="password" class="oswald">Password</label>
+                    <input type="password" class="form-control" id="password" name="password" required>
+                </div>
+
+                <div class="container d-flex justify-content-center pt-3">
+                    <button type="submit" class="btn btn-primary oswald auth-button ">Log in</button>
+                </div>
+
+                <div class="container d-flex justify-content-center mt-3">
+                    <a href="">
+                        <div class="d-flex flex-row align-items-center google-auth-button p-1 pr-3">
+                            <div class="google-icon-container d-flex justify-content-center align-items-center">
+                                <img src="{{ asset('assets/google_icon.png') }}" alt="Google Icon" class="google-icon">
+                            </div>
+                            <p class="oswald m-0 ml-2">Sign in with Google</p>
+                        </div>
+                    </a>
+                </div>
+
+            </form>
+        </div>
+    
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+
+Route::middleware('guest')->group(function () {
+
+    Route::get('/login', function () {
+        return view('login');
+    })->name('login');
+
+    Route::get('/register', function () {
+        return view('register');
+    })->name('register');
+    
+});
