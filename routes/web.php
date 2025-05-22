23fbe0963796e6d6f17678b0948a317b22e5c7c4
Wesley Aldrich <169461537+wesleyaldrich@users.noreply.github.com>
2025-05-22 15:27:54 +0700

    fix(auth-customer): server refusing to start
    the cause of this issue was because i did not define the route required by the form.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 Route::get('/', function () {
     return view('home');
@@ -15,5 +16,7 @@ Route::middleware('guest')->group(function () {
     Route::get('/register', function () {
         return view('register');
     })->name('show.register');
-
+    
+    Route::post('/login', [AuthController::class, 'login'])->name('login');
+    Route::post('/register', [AuthController::class, 'register'])->name('register');
 });
