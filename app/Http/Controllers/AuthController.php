301885a3f8124c69fa8f293d6688edd9b8baabfb
Wesley Aldrich <169461537+wesleyaldrich@users.noreply.github.com>
2025-07-01 10:24:54 +0700

    chore: add credentials for each role

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -208,7 +208,7 @@ class AuthController extends Controller
         if (Auth::attempt($validatedData)) {
             if (Auth::user()->role === 'restaurant') {
                 $request->session()->regenerate();
-                return redirect()->route('home.restaurant');
+                return redirect()->route('restaurant-home');
             }
 
             Auth::logout();
