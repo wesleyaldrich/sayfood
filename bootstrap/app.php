52640468caebb6a0c79da37af9fb1497a0c1ba48
Teresa Naomi <teresanaomi10@gmail.com>
2025-07-31 15:33:23 +0700

    Delete schedule event

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,38 +1,31 @@
-    <?php
+<?php
 
-    use App\Http\Middleware\TwofactorMiddleware;
-    use App\Http\Middleware\UserRoleMiddleware;
-    use Illuminate\Foundation\Application;
-    use Illuminate\Foundation\Configuration\Exceptions;
-    use Illuminate\Foundation\Configuration\Middleware;
+use App\Http\Middleware\TwofactorMiddleware;
+use App\Http\Middleware\UserRoleMiddleware;
+use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Configuration\Exceptions;
+use Illuminate\Foundation\Configuration\Middleware;
 
-    use App\Schedule\EventScheduler;
-    use Illuminate\Console\Scheduling\Schedule;
-    use Illuminate\Foundation\Console\ScheduleList;
+return Application::configure(basePath: dirname(__DIR__))
+    ->withRouting(
+        web: __DIR__.'/../routes/web.php',
+        commands: __DIR__.'/../routes/console.php',
+        health: '/up',
+    )
+    ->withMiddleware(function (Middleware $middleware) {
+        $middleware->alias([
+            'twofactor' => TwofactorMiddleware::class,
+            'role' => UserRoleMiddleware::class
+        ]);
 
-    return Application::configure(basePath: dirname(__DIR__))
-        ->withRouting(
-            web: __DIR__.'/../routes/web.php',
-            commands: __DIR__.'/../routes/console.php',
-            health: '/up',
-        )
-        ->withMiddleware(function (Middleware $middleware) {
-            $middleware->alias([
-                'twofactor' => TwofactorMiddleware::class,
-                'role' => UserRoleMiddleware::class
-            ]);
-
-            $middleware->web(append: [
-                \Illuminate\Session\Middleware\StartSession::class,
-                \App\Http\Middleware\SetLocale::class,
-            ]);
-            // $middleware->appendToGroup('twofactor', [
-            //     TwofactorMiddleware::class,
-            // ]); 
-        })
-        ->withExceptions(function (Exceptions $exceptions) {
-            //
-        })
-        ->withSchedule(function (Schedule $schedule) {
-            (new \App\Schedule\EventScheduler())($schedule);
-        })->create();
+        $middleware->web(append: [
+            \Illuminate\Session\Middleware\StartSession::class,
+            \App\Http\Middleware\SetLocale::class,
+        ]);
+        // $middleware->appendToGroup('twofactor', [
+        //     TwofactorMiddleware::class,
+        // ]); 
+    })
+    ->withExceptions(function (Exceptions $exceptions) {
+        //
+    })->create();
