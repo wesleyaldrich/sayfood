8664f858066e2735e18b84e5d6fee8bc4a007568
crysta lovita <crystalovitaa@gmail.com>
2025-08-01 11:13:41 +0700

    fix(migration): fix relationship in orders table

--- database/migrations/2025_07_03_134254_create_orders_table.php
+++ database/migrations/2025_07_03_134254_create_orders_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
             
-            $table->foreignId('customer_id')->constrained('users')->cascadeOnDelete();
+            $table->foreignId('customer_id')->constrained('customers')->cascadeOnDelete();
             $table->foreignId('restaurant_id')->constrained('restaurants')->cascadeOnDelete();
             
             $table->enum('status', [
